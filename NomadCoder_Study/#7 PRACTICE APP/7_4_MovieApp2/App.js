5ade21db7e0417e10f3262ea777ccb5f5062f12a
SungKim96 <120751395+SungKim96@users.noreply.github.com>
2023-09-04 16:50:02 -0400

    Create App.js

---
+++ NomadCoder_Study/#7 PRACTICE APP/7_4_MovieApp2/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+export default App;
