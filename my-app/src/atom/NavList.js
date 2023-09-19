224a2b58448c2818f546fb2504e277fa78a3728d
SungKim96 <120751395+SungKim96@users.noreply.github.com>
2023-09-19 16:48:58 -0400

    Create NavList.js

---
+++ my-app/src/atom/NavList.js
@@ -0,0 +1,12 @@
+// Group_key_arr : array about all the Keys in Group_obj
+
+const Group_obj = {
+  Action: 'genre=action',
+  Animation: 'genre=animation',
+  Comedy: 'genre=comedy',
+  Romance: 'genre=romance',
+  'Sci-Fi': 'genre=sci-fi',
+};
+const Group_key_arr = Object.keys(Group_obj);
+
+export { Group_obj, Group_key_arr };
